d960cc6ef640dc58fd726e84ebeabda0b78838f5
TheMysticLynx <57817285+TheMysticLynx@users.noreply.github.com>
2022-10-28 22:33:06 -0700

    Project config

---
+++ Myst/src/Myst.h
@@ -0,0 +1,17 @@
+//
+// Created by Kaiden Howard on 10/28/22.
+//
+
+
+// FOR APPLICATIONS
+#ifndef MYST_MYST_H
+#define MYST_MYST_H
+
+#include "Myst/Application.h"
+
+// -------------------
+// --- ENTRY POINT ---
+// -------------------
+#include "Myst/EntryPoint.h"
+
+#endif //MYST_MYST_H
